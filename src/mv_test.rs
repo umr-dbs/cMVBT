6cbcbe5031c45b733074858038828b17f80d4fb7
Amir El-Shaikh <elshaikh@mathematik.uni-marburg.de>
2025-03-06 10:57:41 +0100

    Fix total running time by joining after signaling workload stop.

--- src/mv_test.rs
+++ src/mv_test.rs
@@ -103,7 +103,7 @@ pub fn olap(olap_id: u64, handler: IndexHandler, number_olaps: usize, n: usize)
                 = uni_form.sample(&mut rand::rng()) as RangeMax;
 
             let sleep_time
-                = rand::random_range(olap..(olap+olap_id)*1u64);
+                = rand::random_range(olap..(olap+olap_id)*5u64);
 
             thread::sleep(Duration::from_millis(sleep_time));
             
@@ -378,6 +378,8 @@ pub fn execute_experiments() {
                     }).collect_vec();
 
                 terminate_workload.map(|shutdown| shutdown.store(true, SeqCst));
+                index_handler = Some(sp_index_handler.join().unwrap());
+                
                 total_running_time = SystemTime::now()
                     .duration_since(start_time)
                     .unwrap()
@@ -405,8 +407,9 @@ pub fn execute_experiments() {
                         .unwrap();
                 }
             }
-
-            let index_handler = sp_index_handler.join().unwrap();
+            let mut index_handler 
+                = index_handler.unwrap();
+            
             let (h, r) = height_root(&index_handler);
             let (alloc, reuse) = block_alloc_reuses(&index_handler);
             let (olap_w, olaps_per_w, olaps_joint_workload)
@@ -474,6 +477,8 @@ pub fn execute_experiments() {
                             }).collect_vec();
 
                         terminate_workload.map(|shutdown| shutdown.store(true, SeqCst));
+                        index_handler = sp_index_handler.join().unwrap();
+                        
                         total_running_time
                             = SystemTime::now().duration_since(start_time).unwrap().as_millis();
 
@@ -497,8 +502,7 @@ pub fn execute_experiments() {
                     }
 
                     // drop(olap_handle.take());
-
-                    let index_handler = sp_index_handler.join().unwrap();
+    
                     let (h, r) = height_root(&index_handler);
                     let (alloc, reuse) = block_alloc_reuses(&index_handler);
                     let (olap_w, olaps_per_w, olaps_joint_workload)
@@ -827,6 +831,13 @@ fn experiment(
     };
 
     type WorkerSignal = ();
+    
+    let is_nop =
+        insert_ratio == 0 &&
+        delete_ratio == 0 &&
+        update_ratio == 0 &&
+        points_reads_ratio == 0 &&
+        range_reads_ratio == 0;
 
     let handles = (0..num_threads)
         .map(|_| {
@@ -876,6 +887,7 @@ fn experiment(
                 loop {
                     match thread_control.try_recv() {
                         Err(TryRecvError::Disconnected) => break,
+                        _ if is_nop => thread::sleep(Duration::from_millis(1)),
                         _ => {
                             let next
                                 = local_tx(sampler.sample());
